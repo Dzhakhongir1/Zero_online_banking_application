e20b9aeaa20f21d0ab6d27ff71e52a2a4435e3d0
Jahongir <islamovjaha7@gmail.com>
2021-07-28 14:12:39 -0500

    Added new dependensies and plug ins to do the parallel testing

--- src/test/java/com/zerobank/runner/CukesRunner.java
+++ src/test/java/com/zerobank/runner/CukesRunner.java
@@ -9,12 +9,13 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {
                 "html:target/cucumber-report.html",
-
+                "json:target/cucumber.json",//jason stands for java scrip object notation
+                "rerun:target/rerun.txt"
         },
         features = "src/test/resources/features",
         glue = "com/zerobank/stepdefinition",
-        dryRun = false,
-        tags = "@wip1"
+        dryRun = false
+
 
 
 )
--- src/test/java/com/zerobank/utilities/Driver.java
+++ src/test/java/com/zerobank/utilities/Driver.java
@@ -3,6 +3,7 @@ package com.zerobank.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,34 +18,35 @@ public class Driver {
     making our driver instance so that its not reachable from outside of the class we make it static
     because we want it to rin before everything else , and also we will use it in static method
      */
-    private static WebDriver driver;
+    private static ThreadLocal<WebDriver> driverPool= new ThreadLocal<>();
 
     /*
     creating re-usable utility method that will return same driver instace everytime we call it
      */
     public static WebDriver getDriver() {
-        if (driver == null) {
+        if (driverPool.get() == null) {
             //we read our browser type from configuration.propertris file ising
             //.get propertrires method we creating ConfigurationReader class.
             String browserType = ConfigurationReader.getProperty("browser");
             switch (browserType) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    driverPool.set(new ChromeDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    driverPool.set(new FirefoxDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
             }
         }
         /*
         same driver instance will be returned every time we call.getDriver(); method
          */
-        return driver;
+        return driverPool.get();
     }
 
     /*
@@ -52,9 +54,9 @@ public class Driver {
     Ether null or not null ust exit
      */
     public static void closeDriver() {
-        if (driver != null) {
-            driver.quit();
-            driver = null;
+        if (driverPool.get() != null) {
+            driverPool.get().quit();
+            driverPool.remove();
         }
     }
 }
